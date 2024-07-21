a34db3c765e6cc70c4c6e282e833fdbf54576eab
kongguangjun <657406719@qq.com>
2024-07-21 13:30:06 +0800

    feat:修改配置

--- .umirc.ts
+++ .umirc.ts
@@ -6,4 +6,9 @@ export default defineConfig({
     { path: "/docs", component: "docs" },
   ],
   npmClient: 'pnpm',
+  plugins: [
+    'umi-plugin-gh-pages'
+  ],
+  base: '/umi4Demo/',
+  publicPath: '/umi4Demo/',
 });
